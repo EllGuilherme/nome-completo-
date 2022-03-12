9a3148cacea5df951544e0091e207e97fb680ae1
EllGuilherme <grds1000gmail.com>
2022-03-12 18:34:28 -0300

    finalizar tarefa

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string nome, sobrenome, sobrenomeemmaiusculo;
+Console.Write("digite seu primeiro nome:");
+nome = Console.ReadLine()!;
+
+Console.Write("digete seu segundo nome: ");
+sobrenome = Console.ReadLine()!;
+
+sobrenomeemmaiusculo = sobrenome.ToUpper();
+
+Console.WriteLine($"nome completo: {nome} {sobrenome}");
+Console.WriteLine($"nome de catalogo: {sobrenomeemmaiusculo}, {nome}");
